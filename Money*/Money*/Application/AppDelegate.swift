e13a2b3c36d528b9e89d7d664d610bc3602d6f02
Tran Duc Tan <tran.duc.tanb@framgia.com>
2019-03-13 11:07:09 +0700

    Task #8266: Apply MVC Structure
    - Initialize the project with MVC Architecture
    - Include SwiftLint

---
+++ Money*/Money*/Application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Money*
+//
+//  Created by tran.duc.tan on 3/13/19.
+//  Copyright © 2019 tranductanb. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
---
+++ Money*/Money*/Scenes/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Money*
+//
+//  Created by tran.duc.tan on 3/13/19.
+//  Copyright © 2019 tranductanb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
